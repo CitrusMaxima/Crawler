0413d47a62c39624441c603a650d6c7263c454be
CitrusMaxima <2663753900@qq.com>
2018-04-01 12:51:46 +0800

    提取电影标题和海报图片链接

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -1,7 +1,5 @@
 package crawler;
 
-import crawler.ConnectionUtil;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,6 +15,31 @@ public class Crawler {
 
         StringBuilder movieInfo=new StringBuilder();
 
+        //提取出电影信息
+        Pattern mainPattern = Pattern.compile("div id=\"content\">\\w*.+\\s*<div id=\"celebrities");
+        Matcher mainMatcher = mainPattern.matcher(targetStr);
+        String mainInfo = "";
+        while (mainMatcher.find()) {
+            mainInfo = mainMatcher.group();
+
+            //提取电影标题
+            Pattern pattern1 = Pattern.compile("v:itemreviewed\">[\\u4e00-\\u9fa5a-zA-Z0-9 ]+");
+            Matcher matcher1 = pattern1.matcher(mainInfo);
+            while (matcher1.find()) {
+                String title = matcher1.group().substring(16);
+                movieInfo.append(title+"\n");
+            }
+
+            //提取海报图片链接
+            Pattern pattern2 = Pattern.compile("img src=\"[a-zA-z]+://[^\\s]* title=\"点击看更多海报");
+            Matcher matcher2 = pattern2.matcher(mainInfo);
+            while (matcher2.find()) {
+                String imgURL = matcher2.group().substring(9,matcher2.group().length()-16);
+                movieInfo.append("海报链接：" + imgURL + "\n");
+            }
+
+        }
+        //movieInfo.append(mainInfo);
         return movieInfo.toString();
     }
 
